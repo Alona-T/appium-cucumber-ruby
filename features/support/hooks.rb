003972a6df424983c86c2f6dac2f6dc49adf6bda
Alona <Alona>
2020-05-01 19:49:16 +0400

    Added screenshot support

--- features/support/hooks.rb
+++ features/support/hooks.rb
@@ -1,7 +1,14 @@
 Before do
-    $driver.start_driver
+  $driver.start_driver
 end
 
-After do
-    $driver.driver_quit
+After do |scenario|
+  if scenario.failed?
+    file_path = 'screenshot.png'
+    driver.save_screenshot(file_path)
+    image = open(file_path, 'rb', &:read)
+    encoded_image = Base64.encode64(image)
+    embed(encoded_image, 'image/png;base64', 'SCREENSHOT')
+  end
+  $driver.driver_quit
 end
