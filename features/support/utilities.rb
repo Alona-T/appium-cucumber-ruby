f39456f79416af6c1efd99a30a363c2c38e0e124
Alona <alona@Alonas-MacBook-Pro.local>
2020-05-27 09:59:39 +0400

    Typo error fi

--- features/support/utilities.rb
+++ features/support/utilities.rb
@@ -18,20 +18,20 @@ module Helper
     elem.click
   end
 
-  def swipe_to_top(element, direction)
+  def swipe_to_top(element, duration)
     elem = element
     x = elem.location.x
     y = elem.location.y
-    Appium::TouchAction.swipe(start_x: x, start_y: y, direction: direction)
+    Appium::TouchAction.swipe(start_x: x, start_y: y, duration: duration)
   end
 
-  def swipe_horizontally(element_s, element_e, direction)
+  def swipe_horizontally(element_s, element_e, duration)
     element_start = element_s
     x_start = element_start.location.x
     y_start = element_start.location.y
     element_end = element_e
     x_end = element_end.location.x
     y_end = element_end.location.y
-    Appium::TouchAction.swipe(start_x: x_end, start_y: y_end, end_x: x_start, end_y: y_start, direction: direction)
+    Appium::TouchAction.swipe(start_x: x_end, start_y: y_end, end_x: x_start, end_y: y_start, duration: duration)
   end
 end
